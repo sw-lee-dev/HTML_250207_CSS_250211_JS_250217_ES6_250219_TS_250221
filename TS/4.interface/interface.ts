1881bb0cdd8d56de44ad6de5583cc69a160391b7
sw-lee-dev <lsw6212@naver.com>
2025-02-21 16:40:59 +0900

    2025-02-21 16:50

--- TS/4.interface/interface.ts
+++ TS/4.interface/interface.ts
@@ -6,9 +6,55 @@
 */
 
 /*
-    변수(객체)의 타입으로써 인터페이스
+    변수(객체)의 타입으로써 인터페이스 : 타임스크립트에서 90% 이상 사용되는 용도
 */
+interface King {
+    name: string;
+    ho: string;
+    age: number;
+}
 
+interface Human {
+    name: string;
+    age: number;
+    address: string;
+}
+
+const taejo: King = {
+    name: '이성계',
+    ho: '태조',
+    age: 74
+};
+
+const jeongjong: Human = {
+    name: '이방과',
+    age: 63,
+    address: '고려'
+};
+
+interface King2 extends Human {
+    name: string;
+    ho: string;
+    age: number;
+}
+
+interface Human2 extends King {
+    name: string;
+    age: number;
+    address: string;
+}
+
+interface KingHuman extends King, Human {
+    // 자바에서나 쓰는 완벽한 객체지향적인 방법 >> 타임스크립트에서는 완벽하게 X
+}
+
+// & : intersection(교집함)으로 기본 타입에는 사용 불가
+const taejong: King & Human = {
+    name: '이방원',
+    ho: '태종',
+    age: 58,
+    address: '고려'
+};
 
 
 console.log('==================================================');
@@ -16,16 +62,38 @@ console.log('==================================================');
 /*
     클래스의 타입으로써 인터페이스
 */
+interface IMath {
+    add: (x: number, y: number) => number; // 함수의 타입
+}
 
+class CMath implements IMath {
 
+    add(x: number, y: number) {
+        return x + y;
+    };
+}
 
 console.log('==================================================');
 
 /*
     선택적 속성 :
-    - 
+    - ? 키워드로 클래스나 인터페이스(객체)의 속성을 선택적으로 구현하도록 할 수 있음
 */
+interface King3 {
+    name: string;
+    ho?: string;
+    birth: string;
+    death?: string;
+}
 
+const saejong: King3 = {
+    name: '이도',
+    birth: '1397-05-15'
+}
 
+saejong.ho = '세종';
+saejong.death = '1450-04-08';
+
+// saejong.address = '조선'; >> 인터페이스에 없는 속성은 값을 넣을 수 없음
 
 console.log('==================================================');
--- TS/5.type_alias/type_alias.ts
+++ TS/5.type_alias/type_alias.ts
@@ -1,9 +1,76 @@
 /*
     타입 앨리어스 :
-    - 
-    - 
+    - 커스텀 타입을 지정하는 방법 (타입의 별칭 지정, 어딘가에 저장해 놓는 것)
+    - type 키워드를 사용
 */
+// let day: 'am' | 'pm';
+type Day = 'am' | 'pm' | null;
+let day1: Day = 'am';
 
-
+type int = number;
+let number: int = 10;
 
 console.log('==================================================');
+
+// 객체 타입 정의하는 방법
+// 1. interface 이용
+interface IKing {
+    name: string;
+    ho: string;
+    age: number;
+}
+
+const iking: IKing = {
+    name: '이성계',
+    ho: '태종',
+    age: 74
+};
+
+// 2. type (타입 앨리어스) 이용
+type TKing = {
+    name: string;
+    ho: string;
+    age: number;
+}
+
+const tking: TKing = {
+    name: '이방과',
+    ho: '정종',
+    age: 63
+}
+
+// 3. class 이용 (무결성 검사나 클래스 내의 메서드가 필요한 경우에 사용)
+class CKing {
+    constructor(
+        public name: string,
+        public ho: string,
+        public age: number
+    ) {}
+}
+
+const cking1: CKing = {
+    name: '이방원',
+    ho: '태조',
+    age: 56
+}
+
+const cking2: CKing = new CKing('이도', '세종', 54);
+
+const king: IKing = tking;
+
+class C1 {
+    constructor(
+        public a: number,
+        public b: number
+    ) {}
+}
+
+class C2 {
+    constructor(
+        public a: number,
+        public b: number
+    ) {}
+}
+
+const ins1: C1 = new C1(10, 20);
+const ins2: C2 = ins1;
